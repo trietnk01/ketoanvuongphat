2d07ec24e5be4c8f41d62c837bf37ac4c6edc8cc
dienkim <trietnk01@gmail.com>
2019-01-08 20:43:02 +0700

    cập nhật

--- wp-content/themes/redi-ketoanvuongphat/block/block-fanpage.php
+++ wp-content/themes/redi-ketoanvuongphat/block/block-fanpage.php
@@ -1,27 +1,6 @@
 <?php 
 $link_fanpage=get_field('op_link_fanpage','option');
 ?>
-<div class="desktop-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="350" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="desktop-1024-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="280" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="ipad-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="380" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="mobile-horizontal-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="510" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="mobile-vertical-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="380" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="mobile-vertical-360-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="330" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="mobile-vertical-320-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="285" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
-</div>
-<div class="mobile-vertical-240-device">
-	<div class="fb-page" data-href="<?php echo @$link_fanpage; ?>" data-tabs="timeline" data-width="205" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="<?php echo @$link_fanpage; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage; ?>"><?php echo get_bloginfo('name'); ?></a></blockquote></div>	
+<div>
+	<div class="fb-page" data-href="<?php echo @$link_fanpage ?>" data-tabs="timeline" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="<?php echo @$link_fanpage ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo @$link_fanpage ?>">Kế Toán Thuế Vương Phát</a></blockquote></div>
 </div>
